a5dc9439b05b4d0525981e42cf8daef9bdeff6fd
Gerald ATB <gtumbaco074@boscdelacoma.cat>
2026-04-30 12:57:08 +0200

    creacio de la classe jugador

---
+++ models/jugador.ts
@@ -0,0 +1,18 @@
+export class jugador {
+    private _name: string;
+
+    constructor(name: string) {
+        this._name = name;
+    }
+
+    // Getters i Setters
+    get name(): string {
+        return this._name;
+    }
+
+    set name(newName: string) {
+        this._name = newName;
+    }
+
+    // Methods
+}
